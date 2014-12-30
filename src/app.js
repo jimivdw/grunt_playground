b0d3b79e3eee702fd37d10f2e39b4039d9dfb3aa
allensunbo <allensunbo@gmail.com>
2014-12-30 12:05:21 -0500

    fix small issues

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 (function(angular) {
     var app = angular.module('myApp', ['myAppService', 'ui.bootstrap']);
     app.controller('MainCtrl', ['$scope', 'LogService', function($scope, logService) {
-        var vm = this;
+        var vm = this, count = 0;
         vm.message = 'Hello, Grunt with Angular!';
         logService.log('info', vm.message);
         logService.log('error', vm.message);
         logService.log('warn', vm.message);
         vm.change = function() {
-            vm.message = 'Message changed via button click!';
+            vm.message = 'You click '+ (++count) +' time' + (count>1?'s':'') + '!';
         };
 
     }]);
