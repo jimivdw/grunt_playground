55f723580c1e6497083431589e7c8e06d0914ba4
allensunbo <allensunbo@gmail.com>
2015-01-01 15:34:33 -0500

    e2e

---
+++ test/e2e/indexTest.js
@@ -0,0 +1,22 @@
+describe('index page', function() {
+    beforeEach(function() {
+        browser().navigateTo('src/index.html');
+        sleep(1);
+    });
+
+    it('path works', function() {
+        expect(browser().location().path()).toBe('');
+    });
+
+    it('click works', function() {
+        element(':button').click();
+        expect(element('#message').html()).toBe('You click 1 time!');
+        element(':button').click();
+        expect(element('#message').html()).toBe('You click 2 times!');
+    });
+
+    it('rest service works', function() {
+        expect(element('#weather').html()).toContain('London');
+    });
+
+});
